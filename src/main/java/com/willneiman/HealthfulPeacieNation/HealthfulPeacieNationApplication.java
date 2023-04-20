0628562a72d17e0c7c107ca20e64fe25a86c2750
WillNeiman <willneimann@gmail.com>
2023-04-21 05:17:37 +0900

    Initial commit

---
+++ src/main/java/com/willneiman/HealthfulPeacieNation/HealthfulPeacieNationApplication.java
@@ -0,0 +1,13 @@
+package com.willneiman.HealthfulPeacieNation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthfulPeacieNationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthfulPeacieNationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willneiman/HealthfulPeacieNation/HealthfulPeacieNationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willneiman.HealthfulPeacieNation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthfulPeacieNationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
