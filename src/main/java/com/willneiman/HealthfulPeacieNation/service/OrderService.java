52b63e04f7f505a31377bf4591e1dd91e52cb2dc
WillNeiman <122020165+WillNeiman@users.noreply.github.com>
2023-04-26 17:42:50 +0900

    Update OrderService.java

--- src/main/java/com/willneiman/HealthfulPeacieNation/service/OrderService.java
+++ src/main/java/com/willneiman/HealthfulPeacieNation/service/OrderService.java
@@ -1,6 +1,8 @@
 package com.willneiman.HealthfulPeacieNation.service;
 
+import com.willneiman.HealthfulPeacieNation.repository.MemberRepository;
 import com.willneiman.HealthfulPeacieNation.repository.OrderRepository;
+import com.willneiman.HealthfulPeacieNation.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,7 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
+
 }
